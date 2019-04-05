3381d5c164ffa71f0bbe1e0ff5e095e0c54370b3
Julian Harley <julian.harley.fr@harpercollins.co.uk>
2019-04-05 16:10:23 +0100

    drf init commit

--- foodmanager/urls.py
+++ foodmanager/urls.py
@@ -1,8 +1,20 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+
 from barcode_listener.views import listener
+from django.urls import include, path
+from rest_framework import routers
+from barcode_listener import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
-    path('', listener, name='listener'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('boom', listener, name='listener'),
     path('admin/', admin.site.urls),
 ]
