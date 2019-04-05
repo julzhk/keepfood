98c3fca110e87f7738ff8a3e05fa1ca64df2b0ac
Julian Harley <julian.harley.fr@harpercollins.co.uk>
2019-04-05 16:55:08 +0100

    product model update

--- barcode_listener/admin.py
+++ barcode_listener/admin.py
@@ -6,5 +6,15 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
+    list_display = (
+        'id',
+        'brand',
+        'category',
+        'description',
+        'newupc',
+        'title',
+        'type',
+        'upcnumber',
+    )
+    list_filter = ('error', 'created_at', 'modified_at')
+    date_hierarchy = 'created_at'
--- barcode_listener/serializers.py
+++ barcode_listener/serializers.py
@@ -1,10 +1,13 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+
 from .models import Product
+
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
-        fields = ('name', )
-
-
+        fields = ('id', 'age', 'alias', 'brand', 'category',
+                  'color', 'description', 'error', 'gender',
+                  'msrp', 'newupc', 'rate_down', 'rate_up', 'size',
+                  'st0s', 'title', 'type', 'unit', 'upcnumber',
+                  )
